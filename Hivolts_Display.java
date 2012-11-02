f65a67ca366a0f199951c726fea60a7774a84ec0
Athaddius <a_shmidty@hotmail.com>
2012-11-02 12:21:10 -0700

    added direction references switch case for interrupt

--- Hivolts_Display.java
+++ Hivolts_Display.java
@@ -26,8 +26,36 @@ public class Hivolts_Display {
 
 					hivoltsgrid.drawAll();
 
+					switch(input[0]){
+						case 'q':
+							System.out.println("NW");
+							break;
+						case 'w':
+							System.out.println("N");
+							break;
+						case 'e':
+							System.out.println("NE");
+							break;
+						case 'a':
+							System.out.println("W");
+							break;
+						case 's':
+							System.out.println("nop");
+							break;
+						case 'd':
+							System.out.println("E");
+							break;
+						case 'z':
+							System.out.println("SW");
+							break;
+						case 'x':
+							System.out.println("S");
+							break;
+						case 'c':
+							System.out.println("SE");
+							break;
+					}
 //					System.in.skip(System.in.available());
-					System.out.println(input[0]);
 
 				}
 			}catch(IOException e){
